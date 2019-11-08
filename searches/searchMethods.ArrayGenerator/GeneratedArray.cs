b1b0c6817c8421ab3b8f53f467cbb7e0d1523fb9
Paul Okunev <paul.okunev00@gmail.com>
2019-11-08 23:02:00 +0300

    create parameterized findwithbinary test

---
+++ searches/searchMethods.ArrayGenerator/GeneratedArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace searchMethods.ArrayGenerator
+{
+    public class GeneratedArray
+    {
+        public static int[] Generate(int N, int min, int max)
+        {
+            //1. Use template method
+            Random rand = new Random();
+            int[] array = new int[N];
+            for (int i = 0; i < N; i++)  {
+                array[i] = rand.Next(min, max+1);
+            }
+
+            //2. Array won't always be in increasing order, so we should sort it using QuickSort(O(nlogn))
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
--- searches/searchMethods.Tests/searchingTests.cs
+++ searches/searchMethods.Tests/searchingTests.cs
@@ -1,19 +1,19 @@
 using NUnit.Framework;
+using searchMethods.ArrayGenerator;
 
 namespace searchMethods.Tests
 {
     [TestFixture]
     public class searchingTests
     {
-        [Test]
+        [Test]        
         public void FindWithLinearSearch()
         {
             //1. generate array of N size
             
             int[] array = new int[]{-10,1,4,10,15,22,34,35,44,111};
-            int[] array2 = null;
+            //int[] array2 = null;
             SearchMethods sm = new SearchMethods();
-            
             int elemToFind = 22;
 
             int result = sm.LinearSearch(array,elemToFind);
@@ -28,7 +28,7 @@ namespace searchMethods.Tests
         public void FindWithInterpolationSearch()
         {
             int[] array = new int[]{-10,1,4,10,15,22,34,35,44,111};
-            int[] array2 = null;
+            //int[] array2 = null;
             SearchMethods sm = new SearchMethods();
             
             int searchedElement = 44;
@@ -39,19 +39,18 @@ namespace searchMethods.Tests
                                                
         }
 
-        [Test]
-        public void FindWithBinarySearch()
+        //        size    min   max   key
+        [TestCase(10000, -1000, 1000, 700)]
+        public void FindWithBinarySearch(int N, int min, int max, int key)
         {
-            int[] array = new int[]{-10,1,4,10,15,22,34,35,44,111};
-            int[] array2 = null;
-            SearchMethods sm = new SearchMethods();
+            //An object that has randomly generated sorted array of N in [min, max] range
+            int[] array = GeneratedArray.Generate(N,min,max);
+
+            int result = (new SearchMethods()).BinarySearch(array, key);
             
-            int searchedElement = 111;
-
-            int result = sm.BinarySearch(array,searchedElement);
-
-            Assert.AreEqual(result, 9);
-                                               
+            //TODO: create another way of testing
+            //Maybe upload data to some file
+            Assert.AreEqual(result, 9);               
         }
     }
 }
